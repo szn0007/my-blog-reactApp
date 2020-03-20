2bd0b5e4b1e4aea0ed22c59e51c8629e7e0755e1
Sujan Shrestha <szn0007@Sujans-MacBook-Pro.local>
2020-03-20 13:19:39 +0545

    Initial Commit

---
+++ src/build/precache-manifest.a97b70f0d353dbd12bb331bdb201ed4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4fd97d82ec2f4921d0f",
+    "url": "/static/css/main.1e5f6949.chunk.css"
+  },
+  {
+    "revision": "d4fd97d82ec2f4921d0f",
+    "url": "/static/js/main.05e8e690.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4948f098efaa1293f6c",
+    "url": "/static/js/2.e5ea7a1a.chunk.js"
+  },
+  {
+    "revision": "012d5fae7bead27d0eb3948a2a3eec8a",
+    "url": "/index.html"
+  }
+];
